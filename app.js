02005b0ef32ea4ec1bfb75b4a472de3170c7d692
MohammedG91 <mo.gelu@gmail.com>
2024-12-13 14:44:32 +0000

    changed the arrows to buttons with an aria label

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const images = [{}];
+// images to use to add to array
+// https://i.dailymail.co.uk/i/pix/2010/11/04/article-1326705-0BEABA41000005DC-76_634x413.jpg
+
+// https://sportsbyte.sunderland.ac.uk/wp-content/uploads/2023/05/1416166644.0-scaled.jpg
+
+// https://i.pinimg.com/originals/f0/ec/57/f0ec57a192f6e8c49f7d3ca72a12003b.jpg
+
+// https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2172050434.jpg?q=w_1110,c_fill
+
+// https://www.formula1.com/trackside-images/2024/F1_Grand_Prix_of_Bahrain/2053149561.jpg
