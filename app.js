6209a85d4f8cdb0ff36a69aeafb9b4b45728693a
MohammedG91 <mo.gelu@gmail.com>
2024-12-13 15:22:35 +0000

    updated alts to images

--- app.js
+++ app.js
@@ -3,27 +3,27 @@
 const images = [
   {
     src: "https://i.dailymail.co.uk/i/pix/2010/11/04/article-1326705-0BEABA41000005DC-76_634x413.jpg",
-    alt: "A picture of footballer",
+    alt: "A picture of footballer Steven Gerrard making a ferocious tackle",
   },
 
   {
     src: "https://sportsbyte.sunderland.ac.uk/wp-content/uploads/2023/05/1416166644.0-scaled.jpg",
-    alt: "An image, will update",
+    alt: "A picture of Leon Edwards knocking out Kumaru Usman in round 5 with a headkick to win the title",
   },
 
   {
     src: "https://i.pinimg.com/originals/f0/ec/57/f0ec57a192f6e8c49f7d3ca72a12003b.jpg",
-    alt: "An image, will update",
+    alt: "A picure of Lebron James making a historic dunk",
   },
 
   {
     src: "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2172050434.jpg?q=w_1110,c_fill",
-    alt: "An image, will update",
+    alt: "A picture of Canelo Alvarez landing a right hook in a Boxing match title defence",
   },
 
   {
     src: "https://www.formula1.com/trackside-images/2024/F1_Grand_Prix_of_Bahrain/2053149561.jpg",
-    alt: "An image, will update",
+    alt: "A picture off all the cars in Formula 1 at the 2024 Bahrain grand prix",
   },
 ];
 
